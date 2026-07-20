ba3c95732098e13b9b2e8fcc9ba25c2a91ed03f3
delta demo <ishabhat3090@gmail.com>
2026-07-21 00:03:58 +0530

    fixed cors issue

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -14,7 +14,7 @@ const app=express();
 app.use(express.json());
 
 app.use(cors({
-    origin:["http://localhost:5173","https://study-hub-isha-bhat.vercel.app/"
+    origin:["http://localhost:5173","https://study-hub-isha-bhat.vercel.app"
     ],
     credentials:true
 }));
