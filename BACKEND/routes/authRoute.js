39c9dae3b30c231050542d3a79e3b1afcbd39da1
delta demo <ishabhat3090@gmail.com>
2026-08-19 14:22:31 +0530

    route fix

--- BACKEND/routes/authRoute.js
+++ BACKEND/routes/authRoute.js
@@ -1,12 +1,12 @@
 
-const {Signup,Login,Logout,SendOtp,VerifyOtp}=require("../controllers/authController");
+const {Login,Logout,SendOtp,VerifyOtp}=require("../controllers/authController");
 const { userVerification } = require("../middlewares/authMiddleware");
 const router=require("express").Router();
 
 
 router.post("/send-otp",SendOtp);
 router.post("/verify-otp",VerifyOtp);
-router.post("/signup",Signup);
+
 router.post("/login",Login);
 router.get("/verify",userVerification);
 router.post("/logout",Logout);
