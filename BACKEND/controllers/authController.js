4df4577c22ea9bf1a386724717f13e3519dec948
delta demo <ishabhat3090@gmail.com>
2026-08-21 11:35:08 +0530

    signup err

--- BACKEND/controllers/authController.js
+++ BACKEND/controllers/authController.js
@@ -8,42 +8,61 @@ const bcrypt = require("bcryptjs");
 
 module.exports.Signup = async (req, res) => {
   try {
+    const { email, username, password } = req.body;
 
-    const { email, username, password, createdAt } = req.body;
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.json({ success:false,message: "User already exists!" });
+    if (!email || !username || !password) {
+      return res.json({
+        success: false,
+        message: "All fields are required!",
+      });
     }
 
-    if(!email || !username || !password){
-   return res.json({
-      success:false,
-      message:"All fields are required!"
-   });
-}
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.json({
+        success: false,
+        message: "User already exists!",
+      });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = await User.create({
       email,
       username,
       password: hashedPassword,
-      createdAt,
-    });
-    const token = createSecretToken(user._id);
-    res.cookie("token", token, {
-       httpOnly: true,
-  secure: true,
-  sameSite: "None",
+      isVerified: true
     });
 
-       
-    res
-      .status(201)
-      .json({ message: "User signed in successfully", success: true, user });
+    const token = createSecretToken(user._id);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Account created successfully!",
+      user,
+    });
+
   } catch (err) {
-    console.log(err);
+    console.error("Signup Error:", err);
+
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
   }
 };
 
+
+
+       
+ 
 
 
 module.exports.Login = async (req, res) => {
