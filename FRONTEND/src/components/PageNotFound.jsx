eaae4cf8443330b747b055216afed47a11b83c64
delta demo <ishabhat3090@gmail.com>
2026-08-26 09:54:32 +0530

    page not found

--- FRONTEND/src/components/PageNotFound.jsx
+++ FRONTEND/src/components/PageNotFound.jsx
@@ -7,7 +7,7 @@ function PageNotFound() {
         <h2 className="text-2xl font-semibold mt-4">Page Not Found!</h2>
         <p className="text-2xl font-semibold mt-4">Page you are looking for doesn't exist</p>
 
-   <Link to="/"     className="mt-6 px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Home</Link>
+   <Link to="/" className='text-center'>Home</Link>
     </div>
     </> );
 }
